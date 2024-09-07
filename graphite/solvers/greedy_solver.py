9c7e257f3326914d7b59f750088954b998dfa625
tannguyen.finhay <tan.nguyen3@monstar-lab.com>
2024-09-08 01:46:27 +0700

    dasdas

--- graphite/solvers/greedy_solver.py
+++ graphite/solvers/greedy_solver.py
@@ -1,8 +1,35 @@
+# The MIT License (MIT)
+# Copyright © 2023 Yuma Rao
+# Graphite-AI
+# Copyright © 2024 Graphite-AI
+
+# Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated
+# documentation files (the “Software”), to deal in the Software without restriction, including without limitation
+# the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software,
+# and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+
+# The above copyright notice and this permission notice shall be included in all copies or substantial portions of
+# the Software.
+
+# THE SOFTWARE IS PROVIDED “AS IS”, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO
+# THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+# THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+# OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+# DEALINGS IN THE SOFTWARE.
+
+from typing import List, Union
+from graphite.solvers.base_solver import BaseSolver
+from graphite.protocol import GraphProblem
+from graphite.utils.graph_utils import timeout
 import numpy as np
-import random
 import time
 import asyncio
-from typing import List, Tuple, Union
+import random
+
+import bittensor as bt
+
+import numpy as np
+from typing import List, Union
 from graphite.solvers.base_solver import BaseSolver
 from graphite.protocol import GraphProblem
 
@@ -12,21 +39,6 @@ class NearestNeighbourSolver(BaseSolver):
 
     async def solve(self, formatted_problem: List[List[Union[int, float]]], future_id: int) -> List[int]:
         distance_matrix = formatted_problem
-        n = len(distance_matrix[0])
-        best_route = None
-        best_cost = float('inf')
-
-        for _ in range(10):  # Try multiple random starts
-            route = self.nearest_neighbour(distance_matrix, future_id)
-            route = self.two_opt(route, distance_matrix)
-            cost = self.calculate_cost(route, distance_matrix)
-            if cost < best_cost:
-                best_cost = cost
-                best_route = route
-        
-        return best_route
-
-    def nearest_neighbour(self, distance_matrix: List[List[Union[int, float]]], future_id: int) -> List[int]:
         n = len(distance_matrix[0])
         visited = [False] * n
         route = []
@@ -39,54 +51,36 @@ class NearestNeighbourSolver(BaseSolver):
         for _ in range(n - 1):
             if self.future_tracker.get(future_id):
                 return None
+
+            # Find the nearest unvisited neighbor
             nearest_distance = np.inf
-            nearest_node = None
+            nearest_node = -1
+
             for j in range(n):
                 if not visited[j] and distance_matrix[current_node][j] < nearest_distance:
                     nearest_distance = distance_matrix[current_node][j]
                     nearest_node = j
 
-            route.append(nearest_node)
-            visited[nearest_node] = True
-            total_distance += nearest_distance
-            current_node = nearest_node
-        
+            if nearest_node != -1:
+                route.append(nearest_node)
+                visited[nearest_node] = True
+                total_distance += nearest_distance
+                current_node = nearest_node
+
+        # Return to the starting node
         total_distance += distance_matrix[current_node][route[0]]
         route.append(route[0])
         return route
 
-    def two_opt(self, tour: List[int], distance_matrix: List[List[Union[int, float]]]) -> List[int]:
-        best_tour = tour
-        best_cost = self.calculate_cost(best_tour, distance_matrix)
-        improved = True
+    def problem_transformations(self, problem: GraphProblem):
+        return problem.edges
         
-        while improved:
-            improved = False
-            for i in range(1, len(best_tour) - 1):
-                for j in range(i + 1, len(best_tour)):
-                    if j - i == 1:
-                        continue
-                    new_tour = best_tour[:i] + best_tour[i:j][::-1] + best_tour[j:]
-                    new_cost = self.calculate_cost(new_tour, distance_matrix)
-                    if new_cost < best_cost:
-                        best_tour = new_tour
-                        best_cost = new_cost
-                        improved = True
-                        
-        return best_tour
-
-    def calculate_cost(self, tour: List[int], distance_matrix: List[List[Union[int, float]]]) -> float:
-        return sum(distance_matrix[tour[i]][tour[i + 1]] for i in range(len(tour) - 1))
-
-    def problem_transformations(self, problem: GraphProblem) -> List[List[Union[int, float]]]:
-        return problem.edges  
-
-if __name__ == "__main__":
-    # Run the solver on a test MetricTSP
+if __name__=="__main__":
+    # runs the solver on a test MetricTSP
     n_nodes = 100
     test_problem = GraphProblem(n_nodes=n_nodes)
     solver = NearestNeighbourSolver(problem_types=[test_problem.problem_type])
     start_time = time.time()
-    route = asyncio.run(solver.solve(test_problem.edges, future_id=1))
+    route = asyncio.run(solver.solve_problem(test_problem))
     print(f"{solver.__class__.__name__} Solution: {route}")
-    print(f"{solver.__class__.__name__} Time Taken for {n_nodes} Nodes: {time.time() - start_time}")
+    print(f"{solver.__class__.__name__} Time Taken for {n_nodes} Nodes: {time.time()-start_time}")
