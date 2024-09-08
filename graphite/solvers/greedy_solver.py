6d66708a01a352989b7072191923a2049fee2ebb
tannguyen.finhay <tan.nguyen3@monstar-lab.com>
2024-09-08 21:32:05 +0700

    dasdas

--- graphite/solvers/greedy_solver.py
+++ graphite/solvers/greedy_solver.py
@@ -1,79 +1,76 @@
-# The MIT License (MIT)
-# Copyright © 2023 Yuma Rao
-# Graphite-AI
-# Copyright © 2024 Graphite-AI
-
-# Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated
-# documentation files (the “Software”), to deal in the Software without restriction, including without limitation
-# the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software,
-# and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
-
-# The above copyright notice and this permission notice shall be included in all copies or substantial portions of
-# the Software.
-
-# THE SOFTWARE IS PROVIDED “AS IS”, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO
-# THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
-# THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
-# OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
-# DEALINGS IN THE SOFTWARE.
-
 from typing import List, Union
 from graphite.solvers.base_solver import BaseSolver
 from graphite.protocol import GraphProblem
-from graphite.utils.graph_utils import timeout
 import numpy as np
 import time
 import asyncio
 import random
 
-import bittensor as bt
+def path_cost_from_distance_matrix(distances: np.ndarray, path: List[int]) -> float:
+    """Tính tổng khoảng cách của một lộ trình từ ma trận khoảng cách."""
+    return np.sum([distances[path[i], path[i + 1]] for i in range(len(path) - 1)])
 
 class NearestNeighbourSolver(BaseSolver):
-    def __init__(self, problem_types:List[GraphProblem]=[GraphProblem(n_nodes=2), GraphProblem(n_nodes=2, directed=True, problem_type='General TSP')]):
+    def __init__(self, problem_types: List[GraphProblem] = [GraphProblem(n_nodes=2), GraphProblem(n_nodes=2, directed=True, problem_type='General TSP')]):
         super().__init__(problem_types=problem_types)
 
-    async def solve(self, formatted_problem:List[List[Union[int, float]]], future_id:int)->List[int]:
-        distance_matrix = formatted_problem
+    async def solve(self, formatted_problem: List[List[Union[int, float]]], future_id: int) -> List[int]:
+        distance_matrix = np.array(formatted_problem)
         n = len(distance_matrix[0])
-        visited = [False] * n
-        route = []
-        total_distance = 0
 
-        current_node = 0
-        route.append(current_node)
-        visited[current_node] = True
+        best_route = None
+        best_distance = np.inf
 
-        for node in range(n - 1):
-            if self.future_tracker.get(future_id):
-                return None
-            # Find the nearest unvisited neighbour
-            nearest_distance = np.inf
-            nearest_node = random.choice([i for i, is_visited in enumerate(visited) if not is_visited])# pre-set as random unvisited node
-            for j in range(n):
-                if not visited[j] and distance_matrix[current_node][j] < nearest_distance:
-                    nearest_distance = distance_matrix[current_node][j]
-                    nearest_node = j
+        # Khởi tạo từ nhiều điểm bắt đầu khác nhau
+        for start_node in range(n):
+            visited = [False] * n
+            route = []
+            total_distance = 0
 
-            # Move to the nearest unvisited node
-            route.append(nearest_node)
-            visited[nearest_node] = True
-            total_distance += nearest_distance
-            current_node = nearest_node
-        
-        # Return to the starting node
-        total_distance += distance_matrix[current_node][route[0]]
-        route.append(route[0])
-        return route
+            current_node = start_node
+            route.append(current_node)
+            visited[current_node] = True
 
-    def problem_transformations(self, problem: GraphProblem):
+            for _ in range(n - 1):
+                if self.future_tracker.get(future_id):
+                    return None
+                # Tìm điểm gần nhất chưa thăm
+                nearest_distance = np.inf
+                nearest_node = None
+                for j in range(n):
+                    if not visited[j] and distance_matrix[current_node][j] < nearest_distance:
+                        nearest_distance = distance_matrix[current_node][j]
+                        nearest_node = j
+
+                if nearest_node is None:
+                    break
+
+                # Di chuyển đến điểm gần nhất chưa thăm
+                route.append(nearest_node)
+                visited[nearest_node] = True
+                total_distance += nearest_distance
+                current_node = nearest_node
+
+            # Quay lại điểm bắt đầu
+            total_distance += distance_matrix[current_node][start_node]
+            route.append(start_node)
+
+            # Cập nhật lộ trình tốt nhất
+            if total_distance < best_distance:
+                best_distance = total_distance
+                best_route = route
+
+        return best_route
+
+    def problem_transformations(self, problem: GraphProblem) -> List[List[Union[int, float]]]:
         return problem.edges
         
-if __name__=="__main__":
-    # runs the solver on a test MetricTSP
+if __name__ == "__main__":
+    # Chạy solver trên bài toán MetricTSP thử nghiệm
     n_nodes = 100
     test_problem = GraphProblem(n_nodes=n_nodes)
     solver = NearestNeighbourSolver(problem_types=[test_problem.problem_type])
     start_time = time.time()
-    route = asyncio.run(solver.solve_problem(test_problem))
+    route = asyncio.run(solver.solve(test_problem.edges, future_id=1))
     print(f"{solver.__class__.__name__} Solution: {route}")
     print(f"{solver.__class__.__name__} Time Taken for {n_nodes} Nodes: {time.time()-start_time}")
