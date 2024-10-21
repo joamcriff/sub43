9bef4fb4f5b5b0f69451ff08f5158ebca9288c2d
tannguyen.finhay <tan.nguyen3@monstar-lab.com>
2024-10-21 21:29:05 +0700

    123

--- graphite/solvers/lkh_solver.py
+++ graphite/solvers/lkh_solver.py
@@ -43,8 +43,8 @@ class LKHSolver(BaseSolver):
         PRECISION = 1e-04
         RUNS = 1
         INITIAL_TOUR_ALGORITHM = GREEDY
-        KICK_TYPE = 4
-        KICKS = 8
+        KICK_TYPE = 5
+        KICKS = 10
         POPULATION_SIZE = 2
         MAX_TRIALS = {trial}
         TIME_LIMIT = 20
